52480eba223f4c7eb7be810cbc27c365f8dcf8ba
mohsenfennira <mohsen.fennira@esprit.tn>
2023-03-17 02:08:38 +0100

    Services + Fct Get Store By User

--- DeliveryManagement/src/app/services/pickup.service.ts
+++ DeliveryManagement/src/app/services/pickup.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Store } from '../Models/Store';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +9,9 @@ export class PickupService {
 
   constructor(private http:HttpClient) { }
   ///Url Backend
-  url="http://localhost:8081/Pickup/";
+  url="http://localhost:8081/Pickup/RetrieveStoreOfUser";
+
+  getStoreByUser(){
+    return this.http.get<Store[]>(this.url);
+    }
 }
--- DeliveryManagement/src/app/store-list/store-list.component.ts
+++ DeliveryManagement/src/app/store-list/store-list.component.ts
@@ -1,10 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Store } from '../Models/Store';
+import { PickupService } from '../services/pickup.service';
 
 @Component({
   selector: 'app-store-list',
   templateUrl: './store-list.component.html',
   styleUrls: ['./store-list.component.css']
 })
-export class StoreListComponent {
+export class StoreListComponent  {
+  constructor(private pickupService:PickupService,private http:HttpClient){}
+  ngOnInit(){
+     this.getStore();
+  }
+  //////List Store
+  store!:Store[];
+  ////Get All Store by User Id
+  getStore(){
+    this.pickupService.getStoreByUser().subscribe(data=>{this.store=data});
+  }
+
 
 }
