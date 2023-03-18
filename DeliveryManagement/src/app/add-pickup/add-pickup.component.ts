29cb9ee11c88d6456e71506bdaf2584c6410cb85
mohsenfennira <mohsen.fennira@esprit.tn>
2023-03-18 01:09:17 +0100

    Add Choice Liste To Gove + City

--- DeliveryManagement/src/app/add-pickup/add-pickup.component.ts
+++ DeliveryManagement/src/app/add-pickup/add-pickup.component.ts
@@ -49,10 +49,45 @@ export class AddPickupComponent {
   }*/
 
   addForm(_t7: NgForm) {
-    this.pickup.governorate=_t7.controls['gov'].value;
+    this.pickup.governorate=_t7.controls['governorate'].value;
     this.pickup.city=_t7.controls['city'].value;
+    this.pickupService.addPickup(this.pickup,this.idOrder,this.idStore).subscribe
+    (res =>{console.log('Pickup created');this.route.navigateByUrl('/store');})
+  };
 
-    this.pickupService.addPickup(this.pickup,this.idOrder,this.idStore).subscribe(res =>{console.log('Pickup created');this.route.navigateByUrl('/store');})};
+  governorates = [
+    { name: 'Ariana', cities: ['Ariana', 'Raoued', 'Sidi Thabet'] },
+    { name: 'Béja', cities: ['Béja', 'Medjez el-Bab', 'Téboursouk', 'Testour'] },
+    { name: 'Ben Arous', cities: ['Ben Arous', 'Bou Mhel el-Bassatine', 'El Mourouj', 'Ezzahra', 'Hammam Chott', 'Mornag', 'Rades'] },
+    { name: 'Bizerte', cities: ['Bizerte', 'Mateur', 'Menzel Bourguiba', 'Ras Jebel', 'Sejnane', 'Tinja', 'Utique', 'Zarzouna'] },
+    { name: 'Gabès', cities: ['Gabès', 'El Hamma', 'Ghannouch', 'Matmata', 'Métouia', 'Nouvelle Matmata'] },
+    { name: 'Gafsa', cities: ['Gafsa', 'El Ksar', 'Ksar Ghilane', 'Mdhilla', 'Métlaoui', 'Redeyef', 'Sened', 'Sidi Aïch'] },
+    { name: 'Jendouba', cities: ['Jendouba', 'Aïn Draham', 'Balta-Bou Aouane', 'Bou Salem', 'Fernana', 'Ghardimaou', 'Oued Mliz', 'Tabarka'] },
+    { name: 'Kairouan', cities: ['Kairouan', 'Alaa', 'Bou Hajla', 'Chebika', 'Haffouz', 'Oueslatia', 'Sbikha', 'Sidi Bou Ali'] },
+    { name: 'Kasserine', cities: ['Kasserine', 'Sbeitla', 'Thala', 'Foussana', 'Haïdra', 'Hidra', 'Jedelienne', 'Feriana', 'El Ayoun'] },
+    { name: 'Kébili', cities: ['Kébili', 'Douz', 'Faouar', 'Kébili Nord', 'Souk Lahad'] },
+    { name: 'Le Kef', cities: ['Le Kef', 'Dahmani', 'Kalâat Khasba', 'Nebeur', 'Sakiet Sidi Youssef', 'Sers', 'Tajerouine', 'Kalaat Senan'] },
+    { name: 'Mahdia', cities: ['Mahdia', 'Bou Merdes', 'Chebba', 'El Jem', 'Essouassi', 'Hebira', 'Ksour Essef', 'Melloulèche', 'Ouled Chamekh', 'Souassi'] },
+    { name: 'Manouba', cities: ['Manouba', 'Borj El Amri', 'Douar Hicher', 'Mornaguia', 'Oued Ellil', 'Tébourba'] },
+    { name: 'Sousse', cities: ['Sousse', 'Akouda', 'Bouficha', 'Mornaguia', 'Enfidha', 'Hammam Sousse', 'Hergla', 'Kalâa Kebira', 'Kalâa Seghira ', 'Kondar', 'Messaadine', 'Msaken', 'Sidi Bou Ali', 'Sidi El Heni', 'Sousse Jaouhara', 'Sousse Medina', 'Sousse Riadh', 'Sousse Sidi Abdelhamid'] },
+    { name: 'Tunis', cities: ['Tunis', 'Carthage', 'La Goulette', 'Mornaguia', ' La Marsa', 'Sidi Bou Said'] },
+    { name: 'Zaghouan', cities: ['Zaghouan', 'Bir Mcherga', ' Djebel Oust', 'El Fahs', 'Nadhour', 'Saouaf'] },
+  ];
+  selectedGovernorate: string = '';
+  cities: string[] = [];
+  updateCities() {
+    const selectedGov = this.governorates.find(gov => gov.name === this.selectedGovernorate);
+    if (selectedGov) {
+      this.cities = selectedGov.cities;
+    } else {
+      this.cities = [];
     }
+  }
+
+
+
+
+  }
+
 
 
